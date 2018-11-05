8768e05e4ece0631e9e4800c4631574f8c5050ee
Jeremy Frasier <jeremy.frasier@beta.dhs.gov>
2018-11-04 22:43:01 -0500

    Switch to using molecule for tests
    This has the advantage of being able to be run locally.

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,25 @@
+import os
+import pytest
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize('pkg', [
+    'htop'
+])
+def test_packages(host, pkg):
+    package = host.package(pkg)
+
+    assert package.is_installed
+
+
+@pytest.mark.parametrize('file', [
+    '/etc/htoprc'
+])
+def test_files(host, file):
+    f = host.file(file)
+
+    assert f.exists
