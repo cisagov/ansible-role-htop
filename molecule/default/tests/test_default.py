b888d696914db71043bfa72e29a54aae9365ac2d
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-04-22 12:57:39 -0400

    Blacken

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -4,21 +4,18 @@ import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
 
 
-@pytest.mark.parametrize('pkg', [
-    'htop'
-])
+@pytest.mark.parametrize("pkg", ["htop"])
 def test_packages(host, pkg):
     package = host.package(pkg)
 
     assert package.is_installed
 
 
-@pytest.mark.parametrize('file', [
-    '/etc/htoprc'
-])
+@pytest.mark.parametrize("file", ["/etc/htoprc"])
 def test_files(host, file):
     f = host.file(file)
 
